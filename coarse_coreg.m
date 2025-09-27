293ed4748897ceb21a7aa39258364c9e2535a857
emrah onat <eonat87@yahoo.com>
2025-09-28 01:04:43 +0200

    First Commit

--- coarse_coreg.m
+++ coarse_coreg.m
@@ -1,1 +1,10 @@
-%% InSAR Step
+%% MATLAB Code for Coarse Coregistration
+%
+% Coregistration
+% 1 - Coarse Coregistration
+% 2 - Fine Coregistration
+%
+% SAR Interferometry Step
+%
+% Dr. Emrah Onat
+% 28.09.2025
